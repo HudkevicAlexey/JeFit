bbcddf4c5b5b870914b97789e4095d8d6b41d822
Houdini <hudkevic.a@gmail.com>
2020-07-23 16:02:57 +0300

    Fixed issue with JS load

--- src/test/java/pages/myjefit/MyLogsPage.java
+++ src/test/java/pages/myjefit/MyLogsPage.java
@@ -127,7 +127,7 @@ public class MyLogsPage extends BasePage {
     }
 
     public MyLogsPage deleteButtonClick() {
-        WebElement el = driver.findElement(By.xpath("//div[@class='circle-delete-button']//div[contains(text(),'×')]"));
+        WebElement el = driver.findElement(By.xpath("//div[@class='circle-delete-button']//div[contains(text(),'x')]"));
         executor.executeScript("arguments[0].click();", el);
         return this;
     }
